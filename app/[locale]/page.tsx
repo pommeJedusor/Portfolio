4e7335ba743f376858d7064505e6044b57ccf152
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-08-20 11:06:28 +0200

    navbar

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,13 @@
-import { text } from "@/languages_text/text";
+import NavBar from "@/components/NavBar";
+import { Locale, text } from "@/languages_text/text";
 import { generateStaticParams } from "@/utils/generateStaticParams";
 
 
-export default async function Home({params}: {params: Promise<{locale: string}>}) {
-  const {locale} = await params as {locale: "en"|"fr"};
+export default async function Home({params}: {params: Promise<{locale: Locale}>}) {
+  const {locale} = await params;
   return (
     <div>
+      <NavBar locale={locale}/>
       <h1>{ text[locale]["Homepage"]["title"] }</h1>
     </div>
   );
--- languages_text/text.ts
+++ languages_text/text.ts
@@ -1,12 +1,41 @@
+export type Locale = "en"|"fr";
 export const text = {
   "en": {
     "Homepage": {
       "title": "Hello!"
+    },
+    "Navbar": {
+      "short": {
+        "about": "About Me",
+        "projects": "My Projects",
+        "skills": "My Skills",
+        "contact": "Contact Me",
+      },
+      "long": {
+        "about": "About",
+        "projects": "Projects",
+        "skills": "Skills",
+        "contact": "Contact",
+      }
     }
   },
   "fr": {
     "Homepage": {
       "title": "Salout!"
+    },
+    "Navbar": {
+      "short": {
+        "about": "À Propos De Moi",
+        "projects": "Mes Projets",
+        "skills": "Mes Compétences",
+        "contact": "Me Contacter",
+      },
+      "long": {
+        "about": "À Propos",
+        "projects": "Projets",
+        "skills": "Skills",
+        "contact": "Contact",
+      }
     }
   }
 }
