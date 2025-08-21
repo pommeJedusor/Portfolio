a6ad56a0e2a91b5156659237d9427e00f8ababa9
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-08-21 11:04:39 +0200

    started projects part

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -8,7 +8,19 @@ export default async function Home({params}: {params: Promise<{locale: Locale}>}
   return (
     <div>
       <NavBar locale={locale}/>
-      <h1>{ text[locale]["Homepage"]["title"] }</h1>
+      {/* projects */}
+      <div className="flex flex-col items-center">
+        <h2 className="text-3xl text-center mt-20">My projects</h2>
+        <h3 className="text-2xl text-left w-3/4 m-2 text-red-700 hover:text-red-900"><a href="https://chesspomme.com">Chesspomme</a></h3>
+        <p className="w-3/4 text-lg">it's the first website I deployed and as its name shows it's a chess website, for the techs, it uses nodejs (without any framework like express)
+        and bare html and css for the front (and ejs for the templating), for the db I used mysql which I stop using for futur projects
+        as it's pretty heavy on the server (more so because docker is used), I had to implement some important functionalitties
+        like to check that a move is valid server side, allow players to come back to a unfinished game wether
+        they were connected or not, an elo system and I also installed and connected stockfish on the server to allow players 
+        to play againt it, the code can be found
+        <a href="https://github.com/pommeJedusor/chess_pomme" className="text-red-700 hover:text-red-900"> there</a>
+        </p>
+      </div>
     </div>
   );
 }
