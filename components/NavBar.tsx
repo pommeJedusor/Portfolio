18821da8f91b3be05ae9c8a89c7dd261aca92bad
pommeJedusor <118850425+pommeJedusor@users.noreply.github.com>
2025-08-28 15:40:49 +0200

    moved projects in navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,10 +3,10 @@ import { Locale, text } from "@/languages_text/text";
 export default async function NavBar({locale}: {locale: Locale}) {
   return (
     <div className="text-center text-lg border grid grid-cols-4">
-      <a className="cursor-pointer border hidden md:block">{text[locale]["Navbar"]["short"]["about"]}</a>
-      <a className="cursor-pointer border md:hidden">{text[locale]["Navbar"]["long"]["about"]}</a>
       <a className="cursor-pointer border hidden md:block">{text[locale]["Navbar"]["short"]["projects"]}</a>
       <a className="cursor-pointer border md:hidden">{text[locale]["Navbar"]["long"]["projects"]}</a>
+      <a className="cursor-pointer border hidden md:block">{text[locale]["Navbar"]["short"]["about"]}</a>
+      <a className="cursor-pointer border md:hidden">{text[locale]["Navbar"]["long"]["about"]}</a>
       <a className="cursor-pointer border hidden md:block">{text[locale]["Navbar"]["short"]["skills"]}</a>
       <a className="cursor-pointer border md:hidden">{text[locale]["Navbar"]["long"]["skills"]}</a>
       <a className="cursor-pointer border hidden md:block">{text[locale]["Navbar"]["short"]["contact"]}</a>
